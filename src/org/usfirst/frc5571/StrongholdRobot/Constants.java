59bc9386fe25425bb9cf49498f773b1ec38daec2
BeardenRobotics <bobbygz@aol.com>
2016-02-15 20:43:08 -0500

    Added some constants

--- src/org/usfirst/frc5571/StrongholdRobot/Constants.java
+++ src/org/usfirst/frc5571/StrongholdRobot/Constants.java
@@ -1,10 +1,33 @@
 package org.usfirst.frc5571.StrongholdRobot;
 
 public class Constants {
-	//**********************************************************************************************************
-	public final static double SOME_DOUBLE								= 0.0;
+	//*********************************************************************************
+	// DriveTrain Constants
+	public final static double DRIVE_TRAIN_POSITION_Kp							= 0.0;
+	public final static double DRIVE_TRAIN_POSITION_Ki							= 0.0;
+	public final static double DRIVE_TRAIN_POSITION_Kd							= 0.0;
+	public final static double DRIVE_TRAIN_POSITION_Kf							= 0.0;
+	
+	public final static double DRIVE_TRAIN_VELOCITY_Kp							= 0.0;
+	public final static double DRIVE_TRAIN_VELOCITY_Ki							= 0.0;
+	public final static double DRIVE_TRAIN_VELOCITY_Kd							= 0.0;
+	public final static double DRIVE_TRAIN_VELOCITY_Kf							= 0.0;
 	
 	
-	//**********************************************************************************************************
-	public final static int SOME_INT									= 0;
+	//*********************************************************************************
+	//Shooter Constants
+	public final static double SHOOTER_ANGLE_POSITION_Kp							= 0.0;
+	public final static double SHOOTER_ANGLE_POSITION_Ki							= 0.0;
+	public final static double SHOOTER_ANGLE_POSITION_Kd							= 0.0;
+	public final static double SHOOTER_ANGLE_POSITION_Kf							= 0.0;
+	
+	public final static double SHOOTER_WHEELS_VELOCITY_Kp							= 0.0;
+	public final static double SHOOTER_WHEELS_VELOCITY_Ki							= 0.0;
+	public final static double SHOOTER_WHEELS_VELOCITY_Kd							= 0.0;
+	public final static double SHOOTER_WHEELS_VELOCITY_Kf							= 0.0;
+	
+	public final static int SHOOTER_ANGLE_LOAD_SETPOINT								=   0;
+	public final static int SHOOTER_ANGLE_HIGHSHOT_SETPOINT							=   0;
+	public final static int SHOOTER_ANGLE_LOWSHOT_SETPOINT							=   0;
+	
 }
