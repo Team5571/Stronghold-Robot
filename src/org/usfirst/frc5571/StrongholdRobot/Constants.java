eed567eeae4bacf28dbe517650ba0c6eb574f434
BeardenRobotics <bobbygz@aol.com>
2016-02-15 20:26:28 -0500

    Added a Constant prototype file

---
+++ src/org/usfirst/frc5571/StrongholdRobot/Constants.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc5571.StrongholdRobot;
+
+public class Constants {
+	//**********************************************************************************************************
+	public final static double SOME_DOUBLE								= 0.0;
+	
+	
+	//**********************************************************************************************************
+	public final static int SOME_INT									= 0;
+}
