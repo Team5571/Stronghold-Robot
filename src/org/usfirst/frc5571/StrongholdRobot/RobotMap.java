ffbe223eada5d44633c44cccda2488e1de88400e
BeardenRobotics <bobbygz@aol.com>
2016-02-17 10:23:10 -0500

    Corrected prior change.  Both drive wheels were inverted in RB file. Now only right one is

--- src/org/usfirst/frc5571/StrongholdRobot/RobotMap.java
+++ src/org/usfirst/frc5571/StrongholdRobot/RobotMap.java
@@ -58,7 +58,7 @@ public class RobotMap {
         driveTrainRobotDrive21.setExpiration(0.1);
         driveTrainRobotDrive21.setSensitivity(0.5);
         driveTrainRobotDrive21.setMaxOutput(1.0);
-        driveTrainRobotDrive21.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+
         driveTrainRobotDrive21.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
         shooterLeftBoulderWheel = new CANTalon(5);
         LiveWindow.addActuator("Shooter", "LeftBoulderWheel", shooterLeftBoulderWheel);
