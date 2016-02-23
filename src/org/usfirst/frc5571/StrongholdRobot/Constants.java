f31426344a6e15f3b4ca349206877a5008fde29b
BobbyBeardenRobotics <bobbygz@aol.com>
2016-02-23 13:08:01 -0500

    Merge branch 'master' of https://github.com/Team5571/StrongholdRobot into SensorTestOnRecycleRushRobot
    # Conflicts:
    #	.gitignore
    #	StrongHoldRobot.yaml
    #	build.properties
    #	src/org/usfirst/frc5571/StrongholdRobot/OI.java
    #	src/org/usfirst/frc5571/StrongholdRobot/Robot.java
    #	src/org/usfirst/frc5571/StrongholdRobot/RobotMap.java
    #	src/org/usfirst/frc5571/StrongholdRobot/commands/AutonomousDoNothing.java
    #	src/org/usfirst/frc5571/StrongholdRobot/commands/ShooterAimWithJoystick.java
    #	src/org/usfirst/frc5571/StrongholdRobot/commands/ShooterSetAngleWithJoystick.java
    #	src/org/usfirst/frc5571/StrongholdRobot/subsystems/Shooter.java

--- src/org/usfirst/frc5571/StrongholdRobot/Constants.java
+++ src/org/usfirst/frc5571/StrongholdRobot/Constants.java
@@ -1,16 +1,38 @@
 package org.usfirst.frc5571.StrongholdRobot;
 
 public class Constants {
+	// Xbox Controller 
+	// Note:  Dpad is POV
+	public final static int XBOX_LEFT_X_AXIS					= 0;
+	public final static int XBOX_LEFT_Y_AXIS					= 1;
+	public final static int XBOX_LEFT_JOYSTICK_BUTTON			= 9;
+	public final static int XBOX_RIGHT_X_AXIS					= 4;
+	public final static int XBOX_RIGHT_Y_AXIS					= 5;
+	public final static int XBOX_RIGHT_JOYSTICK_BUTTON			= 10;
+	public final static int XBOX_LEFT_TRIGGER_AXIS				= 2;
+	public final static int XBOX_RIGHT_TRIGGER_AXIS				= 3;
+	public final static int XBOX_A_BUTTON						= 1;
+	public final static int XBOX_B_BUTTON						= 2;
+	public final static int XBOX_X_BUTTON						= 3;
+	public final static int XBOX_Y_BUTTON						= 4;
+	public final static int XBOX_LEFT_BUMPER					= 5;
+	public final static int XBOX_RIGHT_BUMPER					= 6;
+	public final static int XBOX_BACK_BUTTON					= 7;
+	public final static int XBOX_START_BUTTON					= 8;
+	
 	// Xbox Controller Controls Mapping
-	public final static int XBOX_SHOOTING_ANGLE_JOYSTICK		= 5;  // Right hand Y axis
-	public final static int XBOX_DRIVING_MAGNITUDE_JOYSTICK		= 1;  // Left hand Y axis
-	public final static int XBOX_DRIVING_TURN_JOYSTICK			= 0;  // Left hand X axis
-	public final static int ARMS_UP_BUTTON						= 7;  // Back Button
-	public final static int ARMS_DOWN_BUTTON					= 8;  // Start Button
-	public final static int LOW_GEAR_BUTTON						= 5;  // Left Bumper
-	public final static int HIGH_GEAR_BUTTON					= 6;  // Right Bumper
-	public final static int SHOOTER_WHEELS_CAPTURE				= 2;  // A Button
-	public final static int SHOOTER_WHEELS_SHOOT				= 4;  // Y Button
+	public final static int XBOX_SHOOTING_ANGLE_JOYSTICK		= XBOX_RIGHT_Y_AXIS;  
+	public final static int XBOX_DRIVING_MAGNITUDE_JOYSTICK		= XBOX_LEFT_Y_AXIS;
+	public final static int XBOX_DRIVING_TURN_JOYSTICK			= XBOX_LEFT_X_AXIS;
+	public final static int XBOX_ARMS_UP_BUTTON					= XBOX_BACK_BUTTON;
+	public final static int XBOX_ARMS_DOWN_BUTTON				= XBOX_START_BUTTON;		
+	public final static int XBOX_LOW_GEAR_BUTTON				= XBOX_LEFT_BUMPER;
+	public final static int XBOX_HIGH_GEAR_BUTTON				= XBOX_RIGHT_BUMPER;
+	public final static int XBOX_SHOOTER_WHEELS_CAPTURE			= XBOX_A_BUTTON;
+	public final static int XBOX_SHOOTER_WHEELS_SHOOT			= XBOX_Y_BUTTON;
+	public final static int XBOX_AIM_PICKUP						= XBOX_X_BUTTON;
+	public final static int XBOX_AIM_HIGHSHOT					= XBOX_B_BUTTON;
+
 	
 	//*********************************************************************************
 	// DriveTrain Constants
