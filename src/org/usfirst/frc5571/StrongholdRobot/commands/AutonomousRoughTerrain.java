5f6b18edb38623c2825f453ba9ace7fdf2d0af25
BobbyBeardenRobotics <bobbygz@aol.com>
2016-03-31 21:36:29 -0400

    Added Autonomous commands for   Moat   Low Bar   Rough Terrain   Portcullis
    Increased duration of drive forward by 0.5 sec
    Set arms to proper position

---
+++ src/org/usfirst/frc5571/StrongholdRobot/commands/AutonomousRoughTerrain.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc5571.StrongholdRobot.commands;
+
+import org.usfirst.frc5571.StrongholdRobot.Constants;
+import org.usfirst.frc5571.StrongholdRobot.subsystems.Pneumatics;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+/**
+ *
+ */
+public class AutonomousRoughTerrain extends CommandGroup {
+    
+    public  AutonomousRoughTerrain() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	
+    	addSequential(new PneumaticsRaiseArms());
+    	addSequential(new WaitCommand(5));
+    	addSequential(new ShooterAimClosedLoop(Constants.SHOOTER_ANGLE_PARKING_SETPOINT));
+    	addSequential(new AutoDriveCommand(-0.80, 0, 3.5));
+    }
+}
